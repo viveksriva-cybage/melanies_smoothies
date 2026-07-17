94f172ea7af583a78d1e299d2c3d7fd926072d25
viveksriva-cybage <viveksriva@cybage.com>
2026-07-17 11:56:32 +0530

    Fix ingredient selection and API response handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredeients:",
@@ -26,7 +25,9 @@ if ingredients_list:
     # st.write("You selected:", ingredients_list)
     ingredients_string=''
     for fruit_chosen in ingredients_list:
-         ingredients_string+=fruit_chosen+' '
+          ingredients_string+=fruit_chosen+' '
+          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+          sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -39,6 +40,4 @@ values ('""" + ingredients_string + """','""" +name_on_order+"""')"""
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-#st.text(smoothiefroot_response)
+
